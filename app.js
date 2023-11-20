3ff8a68337425271eff057036d243a615cf96a72
kinghiholike <etangoking6@gmail.com>
2023-11-20 13:54:49 +0000

    rate limiter

--- app.js
+++ app.js
@@ -4,25 +4,42 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+const enviroment = process.env;
 const cors = require('cors');
-
+const rateLimit = require('express-rate-limit');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // const corsOptions = require('cors');
+//Rate limiter 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
 
 var app = express();
 
 
 
 app.use(cors({
-origin: '*', 
+origin: ['http://localhost:3000','http://gridxmeter.com/'], 
 credentials: true,
 optionSuccessStatus: 200,}
   
 ));
+app.use(
+  session({
+    secret: enviroment.SECRET_KEY, 
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false ,
+      maxAge: 2 * 60 * 1000}, 
+  })
+)
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(limiter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,6 +66,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
 
 
 const db = require('./db'); // Import the database connection from db.js
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const NodeGeocoder = require('node-geocoder');
 
 const config = process.env;
-const enviroment = process.env;
+
 const validator = require('validator');
 const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv'); // Import dotenv
@@ -15,15 +15,6 @@ const connection = require("../db");
 const e = require('cors');
 const { cookie } = require('request');
 router.use(cookieParser());
-router.use(
-  session({
-    secret: enviroment.SECRET_KEY, 
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false ,
-      maxAge: 30 * 60 * 1000}, 
-  })
-)
 
 dotenv.config();
 
@@ -44,7 +35,9 @@ const options = {
 
   // Optional depending on the providers
   // fetch: customFetchImplementation,
-  apiKey: 'AIzaSyA1b-nVLFTCT4rxGdj5HdTZ3ULOYBYosAM', // for Mapquest, OpenCage, Google Premier
+  //AIzaSyA1b-nVLFTCT4rxGdj5HdTZ3ULOYBYosAM
+  apiKey: 'AIzaSyA1b-nVLFTCT4rxGdj5HdTZ3ULOYBYosAM',
+  //AIzaSyAqaUc4pBP_ZfHAgN8dHk8TS_5NM8otvPg // for Mapquest, OpenCage, Google Premier
   formatter: null // 'gpx', 'string', ...
 };
 //geocoder
@@ -56,89 +49,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-// // Sign-Up route
-// router.post('/signup', async (req, res) => {
-//   const { Username, Password, FirstName,  MeterDRN, LastName, Email, IsActive, RoleName } = req.body;
-
-//   // Validate inputs
-//   if (!Username || !Password || !FirstName || !MeterDRN || !LastName || !Email || !IsActive || !RoleName || !validateEmail(Email)) {
-//     return res.status(400).json({ error: 'Invalid input data' });
-//   }
-
-//   try {
-//     console.log('Received registration request with data:');
-//     console.log('Request Body:', req.body);
-
-//     const hashedPassword = await bcrypt.hash(Password, 10);
-
-//     connection.query(
-//       'INSERT INTO users (Username, Password, FirstName, MeterDRN, LastName, Email, IsActive, RoleName) VALUES (?, ?, ?, ?, ?, ?, ? ,?)',
-//       [Username, hashedPassword, FirstName, LastName, Email, IsActive, RoleName],
-//       (err, result) => {
-//         if (err) {
-//           console.error('Registration error:', err);
-//           return res.json(500).json({ error: 'Registration failed', err });
-//         }
-//         console.log('Registration successful');
-
-//         // Define the 'title' variable for the template
-//         // const title = 'Signup Page';
-
-//         // // Pass 'title' to the template
-//         // res.render('your_template', { title: title });
-//       }
-//     );
-//   } catch (error) {
-//     console.error('Error during registration:', error);
-//     res.status(500).json({ error: 'Registration failed', err });
-//   }
-
-
-// });
-
-
-
-// ///////
-
-// //Get User_Profile
-
-// const findUserQuery = 'SELECT Longitude, Lat  FROM MeterLocationInfoTable WHRE meterdrn =  ?' ;
-  
-
-// connection.query(findUserQuery, [MeterDRN], (err, results) => {
-//   if (err) {
-//     return res.status(500).json({ error: 'Database query failed', err });
-//   }
-
-//   if (results.length === 0) {
-//     return res.status(404).json({ error: 'User not found' });
-//   }
-
-//   const user = results[0];
-//   const latitude = user.latitude;
-//   const longitude = user.longitude;
-
-//   // Perform reverse geocoding to get location information
-//   NodeGeocoder.reverse({ lat: latitude, lon: longitude })
-//     .then((geocodingRes) => {
-//       const address = geocodingRes[0].formattedAddress;
-
-//       // Include additional user information in the response
-//       const userData = {
-//         MeterDRN,
-//         address,
-//       };
-
-     
-
-//       // Send the response to the client
-//       res.status(200).json({ message: 'User profile pulled succesfully', userData });
-//     })
-//     .catch((geocodingError) => {
-//       console.error('Error fetching location:', geocodingError);
-//       res.status(500).json({ error: 'Error fetching location', details: geocodingError.message });
-//     });
-// });
 
 
 
@@ -178,16 +88,15 @@ router.post('/signup', async (req, res) => {
 // Sign-In route
 router.post('/signin', (req, res) => {
   const { Email, Password } = req.body;
-  console.log(Email,Password);
+  console.log(Email, Password);
 
-  // Find the user by Username
+  // Find the user by Email
   const findUserQuery = 'SELECT * FROM users WHERE email = ?';
   connection.query(findUserQuery, [Email], (err, results) => {
     if (err) {
       console.error('Error querying the database:', err);
       console.log(err);
       return res.status(500).json({ error: 'Database query failed', details: err });
-      
     }
 
     if (results.length === 0) {
@@ -213,70 +122,152 @@ router.post('/signin', (req, res) => {
         AccessLevel: user.AccessLevel,
       };
 
-
-      // Generate a JWT with user's AccessLevel
+      // Generate a JWT with user's AccessLevel and other user data
       const token = jwt.sign(
-        { UserID: user.UserID, Email: user.Email, AccessLevel: user.AccessLevel },
-        enviroment.SECRET_KEY,
+        {
+          UserID: user.UserID,
+          Email: user.Email,
+          AccessLevel: user.AccessLevel,
+          // Include other user data as needed
+        },
+        process.env.SECRET_KEY,
         { expiresIn: '30m' }
       );
 
-      // Set the cookie
-      res.cookie('token', token, { httpOnly: true });
-      // console.log(cookie);
-
       // Getting the user profile based on MeterDRN
-      const findUserQuery = 'SELECT Lat, Longitude FROM MeterLocationInfoTable WHERE DRN = ?';
+const findUserQuery = 'SELECT Lat, Longitude FROM MeterLocationInfoTable WHERE DRN = ?';
 
-      connection.query(findUserQuery, [user.MeterDRN], (error, results) => {
-        if (error) {
-          return res.status(500).json({ error: 'Database query failed', details: error });
-        }
+connection.query(findUserQuery, [user.MeterDRN], (error, results) => {
+  if (error) {
+    return res.status(500).json({ error: 'Database query failed', details: error });
+  }
 
-        if (results.length === 0) {
-          return res.status(404).json({ error: 'User not found', details: 'The user with the provided MeterDRN is not found.' });
-        }
+  if (results.length === 0) {
+    return res.status(404).json({ error: 'User not found', details: 'The user with the provided MeterDRN is not found.' });
+  }
 
-        const userLocation = results[0];
-        const latitude = userLocation.Lat;
-        const longitude = userLocation.Longitude;
+  const userLocation = results[0];
+  const latitude = userLocation.Lat;
+  const longitude = userLocation.Longitude;
 
-        // Perform reverse geocoding to get location information
-        geocoder.reverse({ lat: latitude, lon: longitude })
-  .then((geocodingRes) => {
-    const formattedAddress = geocodingRes[0].formattedAddress;
+  // Perform reverse geocoding to get location information
+  geocoder.reverse({ lat: latitude, lon: longitude })
+    .then((geocodingRes) => {
+      const formattedAddress = geocodingRes[0].formattedAddress;
 
-    // components from the formatted address
-    const addressComponents = formattedAddress.split(', ');
+      // components from the formatted address
+      const addressComponents = formattedAddress.split(', ');
 
-    // format of data
-    const [streetName,cityName,countryName] = addressComponents;
+      // format of data
+      const [streetName, cityName, countryName] = addressComponents;
 
-    // additional user information in the response
-    const userData = {
+      // additional user information in the response
+      const userData = {
+        MeterDRN: user.MeterDRN,
+        UserID: user.UserID,
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        Email: user.Email,
+        streetName,
+        cityName,
+        countryName,
+      };
+
+      // Generate a JWT with user's AccessLevel
+      const token = jwt.sign(
+        {
+          UserID: user.UserID,
+          Email: user.Email,
+          AccessLevel: user.AccessLevel,
+          FirstName: user.FirstName,
+          LastName: user.LastName,
+          addressComponents
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: '30m' }
+      );
+
+      // Set the token in a cookie
+      res.cookie('Athorisationfrombackend=', token, {
+        httpOnly: false,
+        credentials: 'include',
+        maxAge: 30 * 60 * 1000, // 30 minutes in milliseconds
+      });
+
+      // Send the response with both token and userData
+      res.status(200).json({
+        message: 'User profile pulled successfully',
+        userData,
+        redirect: (`/protected?token=${encodeURIComponent(token)}`)
+        
+      });
+    })
+    .catch((geocodingError) => {
+      console.error('Error fetching location:', geocodingError);
+      res.status(500).json({ error: 'Error fetching location', details: geocodingError.message });
+    });
+});
+
+    });
+  });
+});
+
+// The /protected route remains the same as before
+router.get('/protected', authenticateToken, (req, res) => {
+  // Access user data from req.user
+  const userData = req.user;
+
+  res.json({ message: 'Protected resource accessed', userData });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = req.query.token || (authHeader && authHeader.split(' ')[1]);
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+
+    next();
+  });
+}
+
+
+//Router to get user data 
+
+router.get('/getUser/:UserID', (req, res) => {
+  const { UserID } = req.params;
+  console.log('UserID:', UserID);
+
+  // Find the user by UserID
+  const findUser = 'SELECT * FROM users WHERE UserID = ?';
+  connection.query(findUser, [UserID], (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+      return res.status(500).json({ error: 'Database query failed', details: err });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'User not found', details: 'The user with the provided email is not found.' });
+    }
+
+    // Omitting sensitive information like passwords before sending the response
+    const user = {
       MeterDRN: user.MeterDRN,
       UserID: user.UserID,
       FirstName: user.FirstName,
       LastName: user.LastName,
       Email: user.Email,
-      streetName,
-      cityName,
-      countryName
     };
 
-    // Sending the response to the client
-    res.status(200).json({ message: 'User profile pulled successfully', userData});
-  })
-  .catch((geocodingError) => {
-    console.error('Error fetching location:', geocodingError);
-    res.status(500).json({ error: 'Error fetching location', details: geocodingError.message });
-  });
-
-      });
-    });
+    res.status(200).json({ message: 'User profile retrieved successfully', user });
   });
 });
-
-
-
 module.exports = router;
